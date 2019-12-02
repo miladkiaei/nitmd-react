5680e9012b63e1e72a6150b2beb20e787d2efc30
Milad Kiani <milad.kiani@snapp.market>
2019-12-03 00:15:33 +0330

    initial commit

---
+++ build/precache-manifest.7570992a898497e221ebf39aa05e3a10.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d24e7f795bd06dc55f6cbb20ba279750",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7249bc3c578920dd5c7d",
+    "url": "/static/css/6.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "f78393a6535339d274f5",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "cab90dec3b737e81fd44",
+    "url": "/static/js/0.c467b626.chunk.js"
+  },
+  {
+    "revision": "1719edaa933e5caeacd7",
+    "url": "/static/js/1.3bd7a496.chunk.js"
+  },
+  {
+    "revision": "4746e8b70b7556a7d8d4",
+    "url": "/static/js/10.e0f3be8a.chunk.js"
+  },
+  {
+    "revision": "c58c0170bc969a5d5023",
+    "url": "/static/js/11.35d66fc4.chunk.js"
+  },
+  {
+    "revision": "9aeaca0567542c4a0921",
+    "url": "/static/js/2.eff4e686.chunk.js"
+  },
+  {
+    "revision": "d55d900b00a1476aefb0",
+    "url": "/static/js/5.582eddeb.chunk.js"
+  },
+  {
+    "revision": "7249bc3c578920dd5c7d",
+    "url": "/static/js/6.62705683.chunk.js"
+  },
+  {
+    "revision": "3e5af2b7db1aa2ea923d",
+    "url": "/static/js/7.cce8bbaf.chunk.js"
+  },
+  {
+    "revision": "48f41acef8775ef26430",
+    "url": "/static/js/8.28b152fe.chunk.js"
+  },
+  {
+    "revision": "0f14cfc0e660f2d91b37",
+    "url": "/static/js/9.31a1ee2b.chunk.js"
+  },
+  {
+    "revision": "f78393a6535339d274f5",
+    "url": "/static/js/main.9752a8fe.chunk.js"
+  },
+  {
+    "revision": "1dacbab8200a6ddd2a90",
+    "url": "/static/js/runtime-main.eebabebb.js"
+  },
+  {
+    "revision": "bd10c03dd1e2d05f7fb91f64d66a4ee7",
+    "url": "/static/media/bg.bd10c03d.jpg"
+  },
+  {
+    "revision": "19a8c07d10048f9c30e6b6a70291e031",
+    "url": "/static/media/logo.19a8c07d.jpg"
+  }
+]);
